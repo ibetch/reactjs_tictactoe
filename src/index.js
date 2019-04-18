2e30d769e35577cd4e6736b3b2c13d8c7e94b0f0
K. Tugangui <ktugangui@gmail.com>
2019-04-18 12:46:49 -0700

    feat(TicTacToe): initiate code
    - add default code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './TicTacToe/Game.js';
 
-import "./styles.css";
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Game />
     </div>
   );
 }
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
