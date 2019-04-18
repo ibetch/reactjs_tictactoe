5036e9d3b38f56698d03840054ba3e0253f8672f
K. Tugangui <ktugangui@gmail.com>
2019-04-18 13:35:51 -0700

    feat(Square): add state

--- src/TicTacToe/Square.js
+++ src/TicTacToe/Square.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
 class Square extends React.Component {
+  /** Initialize state to remember that the square got clicked */
+
+  constructor(props) {
+    /** Call super when defining the constructor of a subclass */
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button className="square" onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
