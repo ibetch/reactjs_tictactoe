52975120138725c44101e1bfa8fdf319f377f80c
K. Tugangui <ktugangui@gmail.com>
2019-04-19 14:06:57 -0700

    tests(enzyme): remove

---
+++ src/TicTacToe/Square.spec.js
@@ -0,0 +1,8 @@
+test('something', () => {
+  expect(true).toEqual(true);
+});
+// describe('[unit of work]', () => {
+//   describe('when [scenario/context]', () => {
+//     it('should [expected behaviour]', () => {});
+//   });
+// });
--- src/TicTacToe/calculateWinner.spec.js
+++ src/TicTacToe/calculateWinner.spec.js
@@ -1,17 +1,14 @@
 import calculateWinner from './calculateWinner.util.js';
 
 describe('The calculateWinner function', () => {
-  describe('when there is not yet a winner', () => {
+  it('should allow the players to keep playing when there is not yet a winner', () => {
     const currentSquares = [null, null, null, null, 'O', 'O', null, null, null];
 
-    it('should allow the players to keep playing', () => {
-      expect(calculateWinner(currentSquares)).toBe(null);
-    });
+    expect(calculateWinner(currentSquares)).toBe(null);
   });
-  describe('when there is a winner', () => {
-    const currentSquares = ['X', 'X', 'X', null, 'O', 'O', null, null, null];
-    it("should return 'X' or 'O'", () => {
-      expect(calculateWinner(currentSquares)).toBe('X');
-    });
+
+  const currentSquares = ['X', 'X', 'X', null, 'O', 'O', null, null, null];
+  it("should return 'X' or 'O' when there is a winner", () => {
+    expect(calculateWinner(currentSquares)).toBe('X');
   });
 });
