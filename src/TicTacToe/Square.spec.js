75454618914797dcba92e4df9674670b3a6efdd4
K. Tugangui <ktugangui@gmail.com>
2019-04-19 16:19:46 -0700

    tests(Square): add Enzyme

--- src/TicTacToe/Square.spec.js
+++ src/TicTacToe/Square.spec.js
@@ -1,8 +1,15 @@
-test('something', () => {
-  expect(true).toEqual(true);
+import React from 'react';
+
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import Square from './Square';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('The Square component', () => {
+  it('should render without crashing', () => {
+    const wrapper = shallow(<Square />);
+    expect(wrapper).toBeDefined();
+  });
 });
-// describe('[unit of work]', () => {
-//   describe('when [scenario/context]', () => {
-//     it('should [expected behaviour]', () => {});
-//   });
-// });
