6ff33f1f98e5bc33668fc7d773725aaa131322fe
K. Tugangui <ktugangui@gmail.com>
2019-04-19 18:41:41 -0700

    tests(Square): shallow rendering

--- src/TicTacToe/Square.spec.js
+++ src/TicTacToe/Square.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import Square from './Square';
@@ -8,8 +8,23 @@ import Square from './Square';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('The Square component', () => {
-  it('should render without crashing', () => {
-    const wrapper = shallow(<Square />);
-    expect(wrapper).toBeDefined();
+  describe('when shallowly rendered', () => {
+    it('should exist', () => {
+      const wrapper = shallow(<Square />);
+      expect(wrapper).toBeDefined();
+    });
+    it('simulates click events', () => {
+      const mockClick = jest.fn();
+      const wrapper = shallow(<Square onClick={mockClick} />);
+
+      wrapper.find('button').simulate('click');
+      expect(mockClick.mock.calls.length).toEqual(1);
+    });
+    it('renders props when passed in', () => {
+      const mockValue = 'X';
+      const wrapper = shallow(<Square value={mockValue} />);
+
+      expect(wrapper.text()).toEqual(mockValue);
+    });
   });
 });
