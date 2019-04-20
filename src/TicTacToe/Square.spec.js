6faeb44db6cccdbc519a3bfda7eb90c829cab3dc
K. Tugangui <ktugangui@gmail.com>
2019-04-19 19:23:35 -0700

    test(Square): full and static render tests

--- src/TicTacToe/Square.spec.js
+++ src/TicTacToe/Square.spec.js
@@ -1,10 +1,11 @@
 import { shallow, mount, render } from 'enzyme';
+import sinon from 'sinon';
 
 import Square from './Square';
 
 describe('The Square component', () => {
   describe('when shallowly rendered', () => {
-    it('should exist', () => {
+    it('should be defined', () => {
       const wrapper = shallow(<Square />);
       expect(wrapper).toBeDefined();
     });
@@ -17,19 +18,48 @@ describe('The Square component', () => {
     });
 
     it('renders props when passed in', () => {
-      const mockValue = 'mock value';
-      const wrapper = shallow(<Square value={mockValue} />);
-      expect(wrapper.text()).toEqual(mockValue);
+      const buttonValue = 'button value';
+      const wrapper = shallow(<Square value={buttonValue} />);
+      expect(wrapper.text()).toEqual(buttonValue);
     });
   });
 
   describe('when fully rendered', () => {
-    const wrapper = mount(<Square />);
-    expect(wrapper).toBeDefined();
+    it('should be defined', () => {
+      const wrapper = mount(<Square />);
+      expect(wrapper).toBeDefined();
+    });
+
+    it('simulates click events', () => {
+      const mockClick = jest.fn();
+      const wrapper = mount(<Square onClick={mockClick} />);
+      wrapper.find('button').simulate('click');
+      expect(mockClick.mock.calls.length).toEqual(1);
+    });
+
+    it('allows us to set props', () => {
+      const wrapper = mount(<Square value="baz" />);
+      expect(wrapper.props().value).toEqual('baz');
+      wrapper.setProps({ value: 'foo' });
+      expect(wrapper.props().value).toEqual('foo');
+    });
   });
 
   describe('when statically rendered', () => {
-    const wrapper = render(<Square />);
-    expect(wrapper).toBeDefined();
+    it('should be defined', () => {
+      const wrapper = render(<Square />);
+      expect(wrapper).toBeDefined();
+    });
+
+    it('renders a button tag', () => {
+      const wrapper = render(<Square />);
+      expect(wrapper.find('button')).toBeDefined();
+    });
+
+    it('renders the value as text', () => {
+      const buttonValue = 'button value';
+      const wrapper = render(<Square value={buttonValue} />);
+      expect(wrapper.text()).toEqual(buttonValue);
+    });
   });
 });
