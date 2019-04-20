dff9b9809e93a854f1c8a567dc08cec4e8d1322e
K. Tugangui <ktugangui@gmail.com>
2019-04-19 22:05:50 -0700

    feat(TicTacToe): Add Time Travel
    - store a history of moves
    - lift state up from the Board component to the Game component

--- src/TicTacToe/Board.js
+++ src/TicTacToe/Board.js
@@ -3,56 +3,20 @@ import Square from './Square';
 import calculateWinner from './calculateWinner.util.js';
 
 class Board extends React.Component {
-  /** Set the Board's initial state to contain an array of 9 nulls corresponding to the 9 squares */
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    };
-  }
-
-  handleClick(i) {
-    /**
-     * Create a copy of the squares array to modify instead of modifying the existing array
-     */
-    const squares = this.state.squares.slice();
-    /**
-     * Return early by ignoring a click if someone has won the game or if a Square is already filled
-     */
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
-  }
-
   /** set square's value attribute to read from the squares array */
   /** onClick, maintain the Board's state */
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
--- src/TicTacToe/Game.js
+++ src/TicTacToe/Game.js
@@ -1,15 +1,63 @@
 import React from 'react';
 import Board from './Board';
+import calculateWinner from './calculateWinner.util';
 
 class Game extends React.Component {
+  /** Access history at top-level */
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    /**
+     * Create a copy of the squares array to modify instead of modifying the existing array
+     */
+    const squares = current.squares.slice();
+    /**
+     * Return early by ignoring a click if someone has won the game or if a Square is already filled
+     */
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board squares={current.squares} onClick={i => this.handleClick(i)} />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
